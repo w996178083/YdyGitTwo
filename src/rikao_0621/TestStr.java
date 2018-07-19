8483a9f4b7475f61d6a5aba4d758cc95bc717d09
w996178083 <996178083@qq.com>
2018-07-19 11:42:57 +0800

    test 003

---
+++ src/rikao_0621/TestStr.java
@@ -0,0 +1,18 @@
+package rikao_0621;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//test rikao_0621 test03
+public class TestStr {
+   public static void main(String[] args) {
+	  String subStr ="北京市人民政府和北京市政协";
+	  String str="北京";
+	  Pattern compile = Pattern.compile(str);
+	  Matcher matcher = compile.matcher(subStr);
+	  int found=0;
+	  while(matcher.find()) {
+		  found++;
+	  }
+	  System.out.println(found);
+}
+}
